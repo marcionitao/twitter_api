0c1e986b1e9a0346b1fb966483650535926a41a1
Márcio <marcionitao@gmail.com>
2024-12-31 11:51:38 +0100

    CORS is enabled for the selected origins

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,13 @@ const app = express()
 
 app.use(helmet()) // helmet serve para proteger os cabeçalhos HTTP e evitar ataques de segurança
 app.use(cors()) // cors serve para permitir a comunicação entre o front e o back
+// CORS is enabled for the selected origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5500',
+//   }),
+// )
+
 app.use(urlencoded({ extended: true })) // permitir que o servidor lide com requisições que contenham dados em formato de URL encoded, como formulários HTML.
 app.use(express.json()) //  o Express.js retorna uma resposta no formato JSON
 
